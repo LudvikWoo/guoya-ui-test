3de06152a82d5a5e3f8fa1a702b4c8a5f83557a5
吴令 <wx_da9ca5d160264260a28c6febefd0c909@git.code.tencent.com>
2019-08-01 12:40:21 +0800

    'first'

---
+++ config/config.py
@@ -0,0 +1,12 @@
+PRJ_ROOT_PATH = 'C:/softwareData/PycharmProjects/guoya-ui-test'
+
+GY_UI_URL = 'http://qa.yansl.com/#/home'
+
+GY_DB_MALL = {
+    'host': 'qa.guoyasoft.com',             
+    'port': 3306,                           
+    'db': 'guoya_virtual_mall_1811',        
+    'user': 'root',                         
+    'passwd': 'Guoya006',                   
+    'charset': 'utf8'                       
+}											
---
+++ test_case/conftest.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# Author : 小吴老师
+# Data ：2019/7/31 14:31
+import pytest
+import os
+from selenium import webdriver
+from tools.base_ui import BaseUI
+
+@pytest.fixture(scope='session')
+def base_ui():
+    driver_path = os.path.join(os.path.dirname(__file__), "../chrome_driver/chromedriver.exe")
+    driver = webdriver.Chrome(driver_path)
+    driver.maximize_window()  # 最大化浏览器
+    driver.implicitly_wait(8)  # 设置隐式时间等待
+    base = BaseUI(driver)
+    yield base
+    driver.quit()
